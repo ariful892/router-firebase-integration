b74f1d45244d83af4c56e72b3f106bc65f5ad9e2
ariful892 <ariful00892@gmail.com>
2022-04-09 23:47:57 +0600

    added onauthstate change to persist the auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import useFirebase from '../useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
             <h3>Please login</h3>
+            <div style={{ margin: "20px" }}>
+                <button onClick={signInWithGoogle}>Google sign in</button>
+            </div>
+            <form >
+                <input type="email" name="" id="" placeholder='Enter your email' />
+                <br />
+                <input type="password" name="" id="" placeholder='Enter password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
+
         </div>
     );
 };
---
+++ src/components/Register/Register.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h2>Please Register now</h2>
+            <form >
+                <input type="text" placeholder='ENter your name' />
+                <br />
+                <input type="email" name="" id="" placeholder='Enter your email' />
+                <br />
+                <input type="password" name="" id="" placeholder='Enter password' />
+                <br />
+                <input type="submit" value="Register" />
+            </form>
+
+        </div>
+    );
+};
+
+export default Register;
